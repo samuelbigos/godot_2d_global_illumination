71433e0e2b9d33dc0992f966a45cb11a224a91cb
samuelbigos <bigos.sam+github@googlemail.com>
2020-08-25 19:05:04 +0100

    Fix texture precision issue, continue to tidy.

--- src/Demo.gd
+++ src/Demo.gd
@@ -18,6 +18,7 @@ var _mouse_spawn = false
 var _moving_debug = false
 var _debug_init_pos = Vector2()
 var _moving_debug_press_start = Vector2()
+var _walls = []
 
 """ PUBLIC """
 
@@ -47,6 +48,9 @@ func _ready():
 	$Screen/Screen.get_material().set_shader_param("u_texture_to_draw", $GI.get_texture())
 	
 	# move the scene to the correct viewport for rendering
+	_walls.append($MainScene/Top)
+	_walls.append($MainScene/Left)
+	_walls.append($MainScene/Right)
 	_main_scene = $MainScene
 	_light = $MainScene/Light
 	remove_child($MainScene)
@@ -72,9 +76,10 @@ func _ready():
 	# setup initial control options
 	_on_DistanceModSlider_value_changed(10.0)
 	_on_RaysPerPixelSlider_value_changed(32)
-	_on_EmissionMultiSlider_value_changed(1.5)
-	_on_EmissionRangeSlider_value_changed(2.0)
+	_on_EmissionMultiSlider_value_changed(2.0)
+	_on_EmissionRangeSlider_value_changed(4.0)
 	_on_EmissionDropoffSlider_value_changed(2.0)
+	_on_WallColourCheck_toggled(true)
 	
 func _process(delta):
 	
@@ -83,16 +88,16 @@ func _process(delta):
 	debug_blocking = debug_blocking or $Controls/Control/Minimise.get_global_rect().has_point(get_global_mouse_position())
 	debug_blocking = debug_blocking or $Controls/Control/Move.get_global_rect().has_point(get_global_mouse_position())
 	
+	_ball_timer -= delta
 	if Input.is_action_pressed("ui_click") and not debug_blocking:
-		if _mouse_spawn:
-			_ball_timer -= delta
+		if _mouse_spawn:			
 			if _ball_timer < 0.0:
 				var ball = ball_scene.instance()
 				_main_scene.add_child(ball)
 				var sprite = ball.get_child(0)
 				var colour = Vector3(_rng.randf(), _rng.randf(), _rng.randf()).normalized()
 				sprite.modulate = Color(colour.x, colour.y, colour.z)
-				sprite.set_emissive(1.0)
+				sprite.set_emissive(0.5)
 				sprite.set_colour(sprite.modulate)
 				ball.position = get_global_mouse_position()
 				_ball_timer = ball_frequency
@@ -167,6 +172,7 @@ func _on_DistanceField_pressed(): $Screen/Screen.get_material().set_shader_param
 func _on_RaysPerPixelSlider_value_changed(value):
 	$GI.set_shader_param("u_rays_per_pixel", value)
 	$Controls/Control/TabContainer/Params/RaysPerPixel/RaysPerPixel.text = String(value)
+	$Controls/Control/TabContainer/Params/RaysPerPixel/RaysPerPixelSlider.value = value
 
 func _on_LightBounceButton_toggled(button_pressed):
 	$GI.set_shader_param("u_bounce", button_pressed)
@@ -178,6 +184,7 @@ func _on_DistanceModSlider_value_changed(value):
 	$GI.set_shader_param("u_dist_mod", value)
 	$DistanceField/Texture.get_material().set_shader_param("u_dist_mod", value)
 	$Controls/Control/TabContainer/Params/DistanceMod/DistanceMod.text = "%05.2f" % value
+	$Controls/Control/TabContainer/Params/DistanceMod/DistanceModSlider.value = value
 
 func _on_FreezeButton_toggled(button_pressed):
 	Engine.time_scale = 1.0 * int(!button_pressed)
@@ -185,6 +192,7 @@ func _on_FreezeButton_toggled(button_pressed):
 func _on_EmissionMultiSlider_value_changed(value):
 	$GI.set_shader_param("u_emission_multi", value)
 	$Controls/Control/TabContainer/Params/EmissionMulti/EmissionMulti.text = "%05.2f" % value
+	$Controls/Control/TabContainer/Params/EmissionMulti/EmissionMultiSlider.value = value
 
 func _on_MouseSpawnButton_toggled(button_pressed):
 	_mouse_spawn = button_pressed
@@ -192,10 +200,12 @@ func _on_MouseSpawnButton_toggled(button_pressed):
 func _on_EmissionRangeSlider_value_changed(value):
 	$GI.set_shader_param("u_emission_range", value)
 	$Controls/Control/TabContainer/Params/EmissionRange/EmissionRange.text = "%05.2f" % value
+	$Controls/Control/TabContainer/Params/EmissionRange/EmissionRangeSlider.value = value
 
 func _on_EmissionDropoffSlider_value_changed(value):
 	$GI.set_shader_param("u_emission_dropoff", value)
 	$Controls/Control/TabContainer/Params/EmissionDropoff/EmissionDropoff.text = "%05.2f" % value
+	$Controls/Control/TabContainer/Params/EmissionDropoff/EmissionDropoffSlider.value = value
 
 func _on_Minimise_pressed():
 	$Controls/Control/TabContainer.visible = !$Controls/Control/TabContainer.visible
@@ -207,7 +217,7 @@ func _on_Move_button_down():
 
 func _on_Info_mouse_entered(extra_arg_0):
 	$Controls/Tooltip.visible = true
-	$Controls/Tooltip.rect_position = get_global_mouse_position() - Vector2($Controls/Tooltip.rect_size.x + 10.0, -10.0)
+	$Controls/Tooltip.rect_position = get_global_mouse_position() - Vector2($Controls/Tooltip.rect_size.x + 10.0, 10.0)
 	
 	if extra_arg_0 == 0:
 		$Controls/Tooltip/Label.text = "Number of rays (light probes) sent out per pixel per frame. Each ray that hits an emissive surface adds to the brightness (and colour) of this pixel."
@@ -227,10 +237,27 @@ func _on_Info_mouse_entered(extra_arg_0):
 		$Controls/Tooltip/Label.text = "Reduce time_scale to 0, also stops the time-based ray direction randomisation, so noise will also freeze."
 	if extra_arg_0 == 8:
 		$Controls/Tooltip/Label.text = "Toggles between the mouse moving the single point light, and spawning multicoloured balls."
+	if extra_arg_0 == 9:
+		$Controls/Tooltip/Label.text = "(A) Coloured walls. (B) White walls. (C) Black walls. The more saturated the colour, the more light of that colour will be bounced. The algorithm could be improved to mix the incoming light with the surface colour. At the moment, walls will only radiate their colour."
 			
 func _on_Info_mouse_exited():
 	$Controls/Tooltip.visible = false
 	$Controls/Tooltip.rect_size = Vector2(0.0, 0.0)
 	$Controls/Tooltip/Label.rect_size = Vector2(0.0, 0.0)
+
+func _on_WallColourCheck_toggled(button_pressed):
+	_walls[0].set_colour(Color.red)
+	_walls[1].set_colour(Color.yellow)
+	_walls[2].set_colour(Color.teal)
+
+func _on_WallWhiteCheck_toggled(button_pressed):
+	_walls[0].set_colour(Color(0.5, 0.5, 0.5, 1.0))
+	_walls[1].set_colour(Color(0.5, 0.5, 0.5, 1.0))
+	_walls[2].set_colour(Color(0.5, 0.5, 0.5, 1.0))
+
+func _on_WallBlackCheck_toggled(button_pressed):
+	_walls[0].set_colour(Color.black)
+	_walls[1].set_colour(Color.black)
+	_walls[2].set_colour(Color.black)
 
 """ PUBLIC """
